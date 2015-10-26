c6f336752e7f165a986bb1901a5818da1becabff
Amelia Johnston <a.m.john102@gmail.com>
2015-10-26 11:21:12 -0400

    initial commit

---
+++ fizzbuzzChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fizzbuzzChallenge
+//
+//  Created by Amelia Johnston on 10/26/15.
+//  Copyright © 2015 Amelia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
